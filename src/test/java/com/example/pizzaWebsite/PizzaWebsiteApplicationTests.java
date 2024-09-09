dd933cb9db441735d8d8d234f297e4a6c61c66af
TanguyRaguenes <tanguy.raguenes@gmail.com>
2024-09-09 12:30:09 +0200

    initialisation du projet

---
+++ src/test/java/com/example/pizzaWebsite/PizzaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
