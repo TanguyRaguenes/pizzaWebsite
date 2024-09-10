96f699182196a710f21a857f770024851b7d76bc
Bertrand <blecleacheni@gmail.com>
2024-09-10 09:20:43 +0200

    2e jour
    partage

--- src/main/java/com/eni/pizzaWebsite/ihm/ProductController.java
+++ src/main/java/com/eni/pizzaWebsite/ihm/ProductController.java
@@ -1,4 +1,20 @@
 package com.eni.pizzaWebsite.ihm;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
+
+    @GetMapping("home")
+    public String home(){return "index";}
+
+    @GetMapping("product-form")
+    public String productForm(){
+        return "product-form";
+    }
+
+
+
+
 }
