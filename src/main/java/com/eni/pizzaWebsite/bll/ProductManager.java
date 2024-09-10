9f734a436937bc6a1c404af37b852c49513fdd8b
Bertrand <blecleacheni@gmail.com>
2024-09-10 11:09:31 +0200

    Oui
    push n°72

--- src/main/java/com/eni/pizzaWebsite/bll/ProductManager.java
+++ src/main/java/com/eni/pizzaWebsite/bll/ProductManager.java
@@ -1,4 +1,19 @@
 package com.eni.pizzaWebsite.bll;
 
+import com.eni.pizzaWebsite.bo.Product;
+import com.eni.pizzaWebsite.dao.IDAOProduct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductManager {
+
+    @Autowired
+    private IDAOProduct productDAO;
+
+
+
+    public void save(Product product) {
+        productDAO.save(product);
+    }
 }
--- src/main/java/com/eni/pizzaWebsite/ihm/ProductController.java
+++ src/main/java/com/eni/pizzaWebsite/ihm/ProductController.java
@@ -1,20 +1,36 @@
 package com.eni.pizzaWebsite.ihm;
 
+import com.eni.pizzaWebsite.bll.ProductManager;
+import com.eni.pizzaWebsite.bo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductManager productManager;
+
     @GetMapping("/")
     public String home(){return "index";}
 
-    @GetMapping("product-form")
+    @GetMapping("/product-form")
     public String productForm(){
         return "product-form";
     }
 
+    @PostMapping("product-form")
+    public String postProductForm(@ModelAttribute Product product, BindingResult bindingResult) {
 
+        productManager.save(product);
+
+        return "redirect:/index";
+    }
 
 
 }
