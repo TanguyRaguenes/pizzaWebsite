50002b2c2d4ac86b3b86704c952e9d2a7296f31a
Bertrand <blecleacheni@gmail.com>
2024-09-10 09:54:04 +0200

    bonsoir
    bonsoir

--- src/main/java/com/eni/pizzaWebsite/PizzaWebsiteApplication.java
+++ src/main/java/com/eni/pizzaWebsite/PizzaWebsiteApplication.java
@@ -1,4 +1,4 @@
-package com.example.pizzaWebsite;
+package com.eni.pizzaWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/pizzaWebsite/PizzaWebsiteApplicationTests.java
+++ src/test/java/com/example/pizzaWebsite/PizzaWebsiteApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pizzaWebsite;
+package com.eni.pizzaWebsite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
