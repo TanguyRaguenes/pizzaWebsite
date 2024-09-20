0b37c0c0fe63c935b8a242a41d1392123ff8d76f
Bertrand <blecleacheni@gmail.com>
2024-09-20 14:08:19 +0200

    ça marche

--- src/main/java/com/eni/pizzaWebsite/configuration/securityConfig.java
+++ src/main/java/com/eni/pizzaWebsite/configuration/securityConfig.java
@@ -23,7 +23,7 @@ public class securityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        JdbcUserDetailsManager jdbcUserDetailsManager= new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
         jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT email, password,1 FROM user WHERE email=?");
 
@@ -37,36 +37,39 @@ public class securityConfig {
         http
                 .authorizeHttpRequests((authorize) -> authorize
 
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/customLogin").permitAll()
-                        .requestMatchers("/authForm").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/vendor/**").permitAll()
-                        .requestMatchers("/images/**").permitAll()
-                        .requestMatchers("/css/**").permitAll()
+                                .requestMatchers("/login").permitAll()
+                                .requestMatchers("/customLogin").permitAll()
+                                .requestMatchers("/authForm").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/vendor/**").permitAll()
+                                .requestMatchers("/images/**").permitAll()
+                                .requestMatchers("/css/**").permitAll()
 
-                        .requestMatchers("/logout").authenticated()
-                        .requestMatchers("/user").authenticated()
-                        .requestMatchers("/").authenticated()
+                                .requestMatchers("/logout").authenticated()
+                                .requestMatchers("/user").authenticated()
+                                .requestMatchers("/").authenticated()
 
-                        .requestMatchers("/products-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
-                        .requestMatchers("/orders-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER", "ROLE_DELIVERYPERSON")
-                        .requestMatchers("/product-form").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/product-form/{id}").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/products-list/delete/{id}").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/create-order").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
-                        .requestMatchers("/orders-list/delete/{id_order}").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/clients-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
-                        .requestMatchers("/client-form").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
-                        .requestMatchers("/cart/remove/{id}").hasAuthority("ROLE_MANAGER")
-                        .requestMatchers("/cart/clear").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/products-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
+//                        .requestMatchers("/orders-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER", "ROLE_DELIVERYPERSON")
+                                .requestMatchers(HttpMethod.GET, "/orders-list/{id_state}").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER", "ROLE_DELIVERYPERSON")
+                                .requestMatchers("/product-form").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/product-form/{id}").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/products-list/delete/{id}").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/create-order").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
+                                .requestMatchers("/orders-list/delete/{id_order}").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/clients-list").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
+                                .requestMatchers("/client-form").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
+                                .requestMatchers("/cart/remove/{id}").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers("/cart/clear").hasAuthority("ROLE_MANAGER")
+                                .requestMatchers(HttpMethod.POST, "/update-order-delivery-date/{id_order}").permitAll()
+                                .requestMatchers("/update-order-state/{id_order}").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/orders-list").permitAll()
+                                .requestMatchers("/orders-edit/{id_order}").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
 
-                        .requestMatchers("/orders-edit/{id_order}").hasAnyAuthority("ROLE_MANAGER", "ROLE_PIZZAMAKER")
 
+                                .requestMatchers("/add-orderDetail-to-order").permitAll()
+                                .requestMatchers("/remove-product-from-order").permitAll()
 
-                        .requestMatchers("/add-orderDetail-to-order").permitAll()
-                        .requestMatchers("/remove-product-from-order").permitAll()
-
-                        .anyRequest().denyAll()
+                                .anyRequest().denyAll()
                 );
 
         //Pour utiliser le fonctionnement de login de Spring Security par défaut
